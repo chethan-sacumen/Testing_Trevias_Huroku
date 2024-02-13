9cd9e0ea03ada7edcf316869e624207a6e313035
chethan-sacumen <chethan.p@sacumen.com>
2024-02-13 15:32:06 +0530

    form sheet

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
+# Flask constructor
 app = Flask(__name__)
 
-@app.route('/')
+# Decorator that tells which url associated with this function
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return "Flask heroku app."
+    """ Home function"""
+    if request.method == 'POST':
+        name = request.form.get('name')
+        place = request.form.get('place')
+        
+        return f'{name.capitalize()}, Welcome to {place}'
+    return render_template('data.html')         
 
-
+# Start the apllication
 if __name__ == '__main__':
-    app.run()
+    app.run(host='localhost',port=5300,debug=True)
+
+
